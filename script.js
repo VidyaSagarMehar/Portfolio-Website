e53f353731faeb35762b549fe6f790355b255110
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-01-15 00:16:25 +0530

    back to top and javascript projects added

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-window.addEventListener("load", async () => {
-	let video = document.querySelector("video[muted][autoplay]");
+window.addEventListener('load', async () => {
+	let video = document.querySelector('video[muted][autoplay]');
 	try {
 		await video.play();
 	} catch (err) {
 		video.controls = true;
 	}
 });
+
+// Get the button
+let mybutton = document.getElementById('btn-back-to-top');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = 'block';
+	} else {
+		mybutton.style.display = 'none';
+	}
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', backToTop);
+
+function backToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
